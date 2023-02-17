f22b953f9e5013570a72044b4952ef13bf21fb9a
ubahq <ubah.info@gmail.com>
2023-02-16 17:25:20 -0800

    aadded file 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This program prints a statement using the puts function
+ * Return: 0 (the program will execute successfully)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
