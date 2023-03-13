c04bddd4c19a13fafc5d1b0703719b06b049c073
ubahq <ubah.info@gmail.com>
2023-03-13 04:02:25 -0700

    try fixing an error

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 
- /**
+/**
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
@@ -10,9 +10,9 @@
  */
 
 int main(int argc __attribute__((unused)), char *argv[])
-
 {
-	printf("%s\n", *argv);
 
+	printf("%s\n", *argv);
 	return (0);
+
 }
